9e02b76d7655054ee3b5aa5c5741d13ae6dd0084
Myriam ANKI <manki@e3r12p19.clusters.42paris.fr>
2021-11-08 17:23:06 +0100

    ADD: module00/ex05/kata0*.py > finished

---
+++ module00/ex05/kata01.py
@@ -0,0 +1,8 @@
+languages = {
+    'Python': 'Guido van Rossum',
+    'Ruby': 'Yukihiro Matsumoto',
+    'PHP': 'Rasmus Lerdorf',
+}
+
+for key, value in languages.items():
+    print("{0} was created by {1}".format(key, value))
