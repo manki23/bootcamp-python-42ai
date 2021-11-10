9afe4aa59c24102b7c85bb7f15a41114868b1d42
Myriam <manki@student.42.fr>
2021-11-10 16:38:05 +0100

    ADD: module01/ex03/generator.py > finished and checked

--- module01/ex03/generator.py
+++ module01/ex03/generator.py
@@ -1,5 +1,28 @@
-import random
+import random  # randint authorized
 
 
-def generator(text, sep=" ", option=None):
+def generator(text, sep, option=None):
     '''Option is an optional arg, sep is mandatory'''
+    if (
+        isinstance(text, str) and
+        option in ["shuffle", "unique", "ordered", None] and
+        isinstance(sep, str) and
+        len(sep) > 0
+    ):
+        split_text = text.split(sep)
+        if option == "shuffle":
+            split_text_copy = split_text.copy()
+            split_text = []
+            while len(split_text_copy) > 0:
+                random_index = random.randint(0, len(split_text_copy) - 1)
+                split_text.append(split_text_copy[random_index])
+                split_text_copy.pop(random_index)
+        elif option == "unique":
+            split_text = list(dict.fromkeys(split_text))
+        elif option == "ordered":
+            split_text.sort()
+
+        for elem in split_text:
+            yield elem
+    else:
+        print("ERROR")
