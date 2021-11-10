7a9ac5affe6f39b936213b02a22a0584e56b09dd
Myriam <manki@student.42.fr>
2021-11-10 21:23:29 +0100

    MOD: module01/ex01/game.py > checked for correction

--- module01/ex01/game.py
+++ module01/ex01/game.py
@@ -1,5 +1,11 @@
 class GotCharacter:
-    def __init__(self, first_name, is_alive=True):
+    """
+        Game of Thrones is an American fantasy drama television series created
+        by David Benioff and D. B. Weiss for HBO. It is an adaptation of A
+        Song of Ice and Fire, a series of fantasy novels by George R. R.
+        Martin, the first of which is A Game of Thrones.
+    """
+    def __init__(self, first_name=None, is_alive=True):
         self.first_name = first_name
         self.is_alive = is_alive
 
