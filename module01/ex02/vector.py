c6387d0589d7afa8f2dfe1d0d00a21423086bf71
Myriam <manki@student.42.fr>
2021-11-12 13:23:41 +0100

    FIX: module01/ex02/vector.py > logic error

--- module01/ex02/vector.py
+++ module01/ex02/vector.py
@@ -3,7 +3,7 @@ import sys
 
 class Vector:
     def is_column_vector(self, values):
-        if (
+        return (
             isinstance(values, list) and
             all(isinstance(value, list) for value in values) and
             not any(
@@ -12,32 +12,23 @@ class Vector:
                 not isinstance(value[0], float)
                 for value in values
             )
-        ):
-            return True
-        else:
-            return False
+        )
 
     def is_row_vector(self, values):
-        if (
+        return (
             isinstance(values, list) and
             all(isinstance(value, float) for value in values)
-        ):
-            return True
-        else:
-            return False
+        )
 
     def is_tuple(self, values):
-        if (
+        return (
             isinstance(values, tuple) and
             len(values) == 2 and
             isinstance(value[0], int) and
             isinstance(value[1], int) and
             values[0] < value[1] and
             values[0] > 0
-        ):
-            return True
-        else:
-            return False
+        )
 
     def print_usage(self):
         print("Vector can be initialized with:")
@@ -68,54 +59,57 @@ class Vector:
             self.print_usage()
             sys.exit()
 
-    def check_vector(self, vector):
-        return self.is_column_vector(vector) or self.is_row_vector(vector)
+    def check_row_vector(self, v1, v2, check_zero=False):
+        return (
+            isinstance(v1, Vector) and
+            isinstance(v2, Vector) and
+            self.is_row_vector(v1.values) and
+            self.is_row_vector(v2.values) and
+            len(v1.values) == len(v2.values) and
+            not (check_zero and any(y == 0 for y in v2.values))
+        )
+
+    def check_column_vector(self, v1, v2, check_zero=False):
+        return (
+            isinstance(v1, Vector) and
+            isinstance(v2, Vector) and
+            self.is_column_vector(v1.values) and
+            self.is_column_vector(v2.values) and
+            len(v1.values) == len(v2.values) and
+            not (check_zero and any(y == 0 for y in v2.values))
+        )
 
     def add(self, v1, v2):
         if self.check_column_vector(v1, v2):
-            return [[x[0] + y[0]] for x, y in zip(v1, v2)]
+            lst = [[x[0] + y[0]] for x, y in zip(v1.values, v2.values)]
+            return Vector(lst)
         elif self.check_row_vector(v1, v2):
-            return [x + y for x, y in zip(v1, v2)]
+            return Vector([x + y for x, y in zip(v1.values, v2.values)])
         else:
             raise ValueError("Cannot add vectors with different sizes")
 
     # add : only vectors of same dimensions.
     def __add__(self, other):
-        self.add(self.values, other)
+        return self.add(self, other)
 
     def __radd__(self, other):
-        self.add(other, self.values)
+        return self.add(other, self)
 
     # sub : only vectors of same dimensions.
     def sub(self, v1, v2):
         if self.check_column_vector(v1, v2):
-            return [[x[0] - y[0]] for x, y in zip(v1, v2)]
+            lst = [[x[0] - y[0]] for x, y in zip(v1.values, v2.values)]
+            return Vector(lst)
         elif self.check_row_vector(v1, v2):
-            return [x - y for x, y in zip(v1, v2)]
+            return Vector([x - y for x, y in zip(v1.values, v2.values)])
         else:
             raise ValueError("Cannot substract vectors with different sizes")
 
     def __sub__(self, other):
-        self.sub(self.values, other)
+        return self.sub(self, other)
 
     def __rsub__(self, other):
-        self.sub(other, self.values)
-
-    def check_row_vector(self, v1, v2, check_zero=False):
-        return (
-            self.is_row_vector(v1) and
-            self.is_row_vector(v2) and
-            len(v1) == len(v2) and
-            not (check_zero and any(y == 0 for y in v2))
-        )
-
-    def check_column_vector(self, v1, v2, check_zero=False):
-        return (
-            self.is_column_vector(v1) and
-            self.is_column_vector(v2) and
-            len(v1) == len(v2) and
-            not (check_zero and any(y == 0 for y in v2))
-        )
+        return self.sub(other, self)
 
     # div : only scalars.
     def __truediv__(self, other):
@@ -124,67 +118,60 @@ class Vector:
             (isinstance(other, float) or isinstance(other, int)) and
             float(other) != 0.0
         ):
-            return [[x[0] / other] for x in self.values]
+            return Vector([[x[0] / other] for x in self.values])
         elif (
             self.is_row_vector(self.values) and
             (isinstance(other, float) or isinstance(other, int)) and
             float(other) != 0.0
         ):
-            return [x / other for x in self.values]
-        elif self.check_column_vector(self.values, other, True):
-            return [[x[0] / y[0]] for x, y in zip(self.values, other)]
-        elif self.check_row_vector(self.values, other, True):
-            return [x / y for x, y in zip(self.values, other)]
+            return Vector([x / other for x in self.values])
+        elif self.check_column_vector(self, other, True):
+            lst = [[x[0] / y[0]] for x, y in zip(self.values, other.values)]
+            return Vector(lst)
+        elif self.check_row_vector(self, other, True):
+            return Vector([x / y for x, y in zip(self.values, other.values)])
         else:
             raise ValueError("__truediv__ can only be used with "
                              + "a scalar different from zero")
 
     def __rtruediv__(self, other):
-        if (
-            self.is_column_vector(self.values) and
-            (isinstance(other, float) or isinstance(other, int)) and
-            all(y != 0 for y in self.values)
-        ):
-            return [[other / y[0]] for y in self.values]
-        elif (
-            self.is_row_vector(self.values) and
-            (isinstance(other, float) or isinstance(other, int)) and
-            all(y != 0 for y in self.values)
-        ):
-            return [other / y for y in self.values]
-        elif self.check_column_vector(other, self.values, True):
-            return [[x[0] / y[0]] for x, y in zip(other, self.values)]
+        if isinstance(other, float) or isinstance(other, int):
+            raise ValueError("A scalar cannot be divided by a Vector")
+        elif self.check_column_vector(other.values, self.values, True):
+            lst = [[x[0] / y[0]] for x, y in zip(other.values, self.values)]
+            return Vector(lst)
         elif self.check_row_vector(other, self.values, True):
-            return [x / y for y in zip(other, self.values)]
+            return Vector([x / y for y in zip(other.values, self.values)])
         else:
             raise ValueError("__rtruediv__ can only be used with "
                              + "a scalar different from zero")
 
-    def mul(self, v1, v2, mul_type):
+    def mul(self, other, mul_type):
         if (
-            self.is_column_vector(v1) and
-            (isinstance(v2, float) or isinstance(v2, int))
+            self.is_column_vector(self.values) and
+            (isinstance(other, float) or isinstance(other, int))
         ):
-            return [[x[0] * v2] for x in v1]
+            return Vector([[x[0] * other] for x in self.values])
         elif (
-            self.is_row_vector(v1) and
-            (isinstance(v2, float) or isinstance(v2, int))
+            self.is_row_vector(self.values) and
+            (isinstance(other, float) or isinstance(other, int))
         ):
-            return [x * v2 for x in v1]
-        elif self.check_column_vector(v1, v2):
-            return [[x[0] * y[0]] for x, y in zip(v1, v2)]
-        elif self.check_row_vector(v1, v2):
-            return [x * y for x, y in zip(v1, v2)]
+            return Vector([x * other for x in self.values])
+        elif self.check_column_vector(self, other):
+            lst = [[x[0] * y[0]] for x, y in zip(self.values, other.values)]
+            return Vector(lst)
+        elif self.check_row_vector(self, other):
+            return Vector([x * y for x, y in zip(self.values, other.values)])
         else:
             raise ValueError(f"{mul_type} can only be used with "
-                             + "a scalar different from zero")
+                             + "a scalar or a vector")
 
     # mul : only scalars.
     def __mul__(self, other):
-        self.mul(self.values, other, "__mul__")
+        return self.mul(other, "__mul__")
 
     def __rmul__(self, other):
-        self.mul(other, self.values, "__rmul__")
+        return self.mul(other, "__rmul__")
 
     def __str__(self):
         return f"Values: {self.values}\nShape: {self.shape}"
@@ -193,10 +180,12 @@ class Vector:
         return f"Vector({self.values})"
 
     def dot(self, other):
-        if self.check_column_vector(self.values, other):
-            return sum([x[0] * y[0] for x, y in zip(v1, v2)])
-        elif self.check_row_vector(v1, v2):
-            return sum([x * y for x, y in zip(v1, v2)])
+        if self.check_column_vector(self, other):
+            return sum(
+                    [x[0] * y[0] for x, y in zip(self.values, other.values)]
+                )
+        elif self.check_row_vector(self, other):
+            return sum([x * y for x, y in zip(self.values, other.values)])
         else:
             raise ValueError(f".dot() can only be used with "
                              + "two vectors of same dimension m")
