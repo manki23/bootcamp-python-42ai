cdc37b454151e21c2ab4f1bdf70b88ed0207f53c
Myriam ANKI <manki@e3r5p19.clusters.42paris.fr>
2021-11-15 15:17:54 +0100

    MOD: module02/ex02/logger.py > use of with context manager instead of f.close()

--- module02/ex02/logger.py
+++ module02/ex02/logger.py
@@ -8,14 +8,13 @@ def log(func):
         start_time = time.time()
         result = func(*args, **kwargs)
         elapsed_time = time.time() - start_time
-        f = open('machine.log', 'a')
         username = os.environ.get('USER')
         elapsed_time_str = f"{elapsed_time:.3f} s"
         if elapsed_time_str == "0.000 s":
             elapsed_time_str = f"{elapsed_time*1000:.3f} ms"
-        f.write(f"({username})Running: {func.__name__}\t"
-                + "[ exec-time = {elapsed_time_str} ]\n")
-        f.close()
+        with open('machine.log', 'a') as f:
+            f.write(f"({username})Running: {func.__name__}\t"
+                    + f"[ exec-time = {elapsed_time_str} ]\n")
         return result
     return wrapper
 
