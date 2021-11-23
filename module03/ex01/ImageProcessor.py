8057b5e36eda9bd9a78c82e32d71f32138f7ef51
Myriam <manki@student.42.fr>
2021-11-23 14:54:24 +0100

    ADD: module03/ex01/ImageProcessor.py > sample code

---
+++ module03/ex01/ImageProcessor.py
@@ -0,0 +1,17 @@
+class ImageProcessor:
+    @staticmethod
+    def load(path):
+        """
+        Opens the .png file specified by the path argument and returns an
+        array with the RGB values of the image pixels It must display a message
+        specifying the dimensions of the image (e.g. 340 x 500)
+        """
+        pass
+
+    @staticmethod
+    def display(array):
+        """
+        Takes a numpy array as an argument and displays the corresponding
+        RGB image
+        """
+        pass
