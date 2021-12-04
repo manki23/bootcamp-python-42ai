6bb49f1c0672d28451a5a5cb0eb548b4d1805723
Myriam ANKI <manki@e1r10p23.42.fr>
2021-12-04 16:34:04 +0100

    ADD: module04/ex00/FileLoader.py > finished

--- module04/ex00/FileLoader.py
+++ module04/ex00/FileLoader.py
@@ -1,8 +1,13 @@
 import sys
+import numpy as np
 import pandas as pd
 
 
 class FileLoader:
+    def __init__(self):
+        pass
+
+    @staticmethod
     def load(path):
         """
         - Takes as an argument the file path of the dataset to load,
@@ -11,14 +16,22 @@ class FileLoader:
         - And returns the dataset loaded as a pandas.DataFrame
         """
         try:
-            return pd.read_csv(path)
+            arr = pd.read_csv(path)
+            print(f"Loading dataset of dimensions {arr.shape[0]} x ",
+                  f"{arr.shape[1]}")
+            return arr
         except Exception as e:
             print(f"{e.__class__.__name__}:", e, file=sys.stderr)
-    
+
+    @staticmethod
     def display(df, n):
         """
         - Takes a pandas.DataFrame and an integer as arguments,
         - Displays the first n rows of the dataset if n is positive,
             or the last n rows if n is negative.
         """
-        pass
+        if isinstance(df, pd.DataFrame) and isinstance(n, int):
+            if n > 0:
+                print(df.head(n))
+            else:
+                print(df.tail(-n))
