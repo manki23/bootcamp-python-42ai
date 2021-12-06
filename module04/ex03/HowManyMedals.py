791d086ec66b1a467fbf04e3b623f077bde639f8
Myriam ANKI <manki@e3r3p8.42.fr>
2021-12-06 09:48:56 +0100

    MOD: module04/ex03/HowManyMedals.py > finished

--- module04/ex03/HowManyMedals.py
+++ module04/ex03/HowManyMedals.py
@@ -3,8 +3,13 @@ import pandas as pd
 
 def howManyMedals(df, name):
     if isinstance(df, pd.DataFrame) and isinstance(name, str):
-        df = df[(df['Name'] ==                name) & df['Medal'].notna()]
+        df = df[(df['Name'] == name)]
         df = df.loc[:, ['Year', 'Medal']]
         dic = df.groupby(['Year'])['Medal'].apply(list).to_dict()
-        for year, value in dic:
-            dic[year]
+        for year, value in dic.items():
+            dic[year] = {
+                'G': dic[year].count('Gold'),
+                'S': dic[year].count('Silver'),
+                'B': dic[year].count('Bronze')
+            }
+        return dic
